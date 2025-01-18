1564a8cd131c783b84e484472176664a32e8709b
vorthrak <elricodaru@gmail.com>
2025-01-18 14:43:44 +0700

    db.php

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$host = "<endpoint database>";
+$dbname = "<nama database>";
+$user = "<username>";
+$password = "<password>";
+
+try {
+    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Could not connect to the database: " . $e->getMessage());
+}
+?>
